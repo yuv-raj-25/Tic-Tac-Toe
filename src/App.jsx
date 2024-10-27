417e4ac9ee654dc3b9dff002c5a7d985918b266f
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2024-10-27 05:57:30 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import TicTacToe from './components/tic-tac-toe.js'
+import TicTacToe from './components/Tic-Tac-Toe.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
